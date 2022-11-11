14c5dd03e67f40b40230c29d5c86df89acfb5d86
AnaelBonnafous <anael.bonnafous@gmail.com>
2022-11-11 11:53:45 +0100

    add app link to dashy + favicon

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ["@nuxt/content"],
+  app: {
+    head: {
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
 });
